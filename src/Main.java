79b124c5228755c84874447060435322c8d8b7ef
Ilya <ibogino@yandex.ru>
2020-01-20 02:12:48 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Main {
+
+    private static int newWidth = 300;
+    public static void main(String[] args){
+
+    String srcFolder = "C:\\Users\\ibogi\\Desktop/src";
+    String dstFolder = "C:\\Users\\ibogi\\Desktop/dst";
+
+    File srcDir = new File(srcFolder);
+
+    long start = System.currentTimeMillis();
+
+    File[] files = srcDir.listFiles();
+
+    int middle = files.length / 2;
+    int fours = files.length - files.length / 4;
+    int quarter = files.length / 4;
+
+   File[] files1 = new File[quarter];
+   System.arraycopy(files, 0, files1, 0, files1.length);
+   ImageResizer resizer1 = new ImageResizer(files1, newWidth, dstFolder, start);
+   new Thread(resizer1).start();
+
+   File[] files2 = new File[quarter];
+   System.arraycopy(files, quarter, files2, 0, files2.length);
+   ImageResizer resizer2 = new ImageResizer(files2, newWidth, dstFolder, start);
+   new Thread(resizer2).start();
+
+   File[] files3 = new File[quarter];
+   System.arraycopy(files, middle, files3, 0, files3.length);
+   ImageResizer resizer3 = new ImageResizer(files3, newWidth, dstFolder, start);
+   new Thread(resizer3).start();
+
+   File[] files4 = new File[quarter];
+   System.arraycopy(files, fours, files4, 0, files4.length);
+   ImageResizer resizer4 = new ImageResizer(files4, newWidth, dstFolder, start);
+   new Thread(resizer4).start();
+
+
+
+    }
+}
