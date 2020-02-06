9097de250dc0624ada4456a997cf80f0b2922538
ibogi <ibogino@yandex.ru>
2020-02-06 09:18:21 +0300

    massive changed to queue

--- src/ImageResizer.java
+++ src/ImageResizer.java
@@ -1,16 +1,18 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ImageResizer implements Runnable{
 
-    private File[] files;
+    private ConcurrentLinkedQueue<File> queue;
     private int newWidth;
     private String dstFolder;
     private long start;
 
-    public ImageResizer(File[] files, int newWidth, String dstFolder, long start) {
-        this.files = files;
+
+    public ImageResizer(ConcurrentLinkedQueue<File> queue, int newWidth, String dstFolder, long start) {
+        this.queue = queue;
         this.newWidth = newWidth;
         this.dstFolder = dstFolder;
         this.start = start;
@@ -20,31 +22,35 @@ public class ImageResizer implements Runnable{
 
     @Override
     public void run() {
-
+        File file;
         try
         {
-            for (File file : files)
+            while (queue.size() > 0)
             {
-                BufferedImage image = ImageIO.read(file);
-                if (image == null) {
-                    continue;
-                }
-
-                int newHeight = (int) Math.round(
-                        image.getHeight() / (image.getWidth() / (double) newWidth));
-                BufferedImage newImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
-
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
+                if ((file = queue.poll()) != null)
+                {
+                    BufferedImage image = ImageIO.read(file);
+                    if (image == null) {
+                        continue;
                     }
+
+                    int newHeight = (int) Math.round(
+                            image.getHeight() / (image.getWidth() / (double) newWidth));
+                    BufferedImage newImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+
+                    int widthStep = image.getWidth() / newWidth;
+                    int heightStep = image.getHeight() / newHeight;
+
+                    for (int x = 0; x < newWidth; x++) {
+                        for (int y = 0; y < newHeight; y++) {
+                            int rgb = image.getRGB(x * widthStep, y * heightStep);
+                            newImage.setRGB(x, y, rgb);
+                        }
+                    }
+                    File newFile = new File(dstFolder + File.separator + file.getName());
+                    ImageIO.write(newImage, "jpg", newFile);
                 }
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage,"jpg", newFile);
+                else return;
             }
         }catch (Exception ex)
         {
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
 
     private static int newWidth = 300;
     public static void main(String[] args){
 
-    String srcFolder = "C:\\Users\\ibogi\\Desktop/src";
-    String dstFolder = "C:\\Users\\ibogi\\Desktop/dst";
+    String srcFolder = "C:\\Users\\Илья\\Desktop\\фото";
+    String dstFolder = "C:\\Users\\Илья\\Desktop\\копия фото";
 
     File srcDir = new File(srcFolder);
 
@@ -17,31 +19,44 @@ public class Main {
 
     File[] files = srcDir.listFiles();
 
+    int cores = Runtime.getRuntime().availableProcessors();
+
+    ConcurrentLinkedQueue<File> queue = new ConcurrentLinkedQueue<>(Arrays.asList(files));
 
 
-    int quarter = (files.length - files.length % 4) / 4;
-    int third = quarter * 3;
-    int middle = quarter * 2;
+    ImageResizer resizer = new ImageResizer(queue, newWidth, dstFolder, start);
 
-   File[] files1 = new File[quarter];
-   System.arraycopy(files, 0, files1, 0, files1.length);
-   ImageResizer resizer1 = new ImageResizer(files1, newWidth, dstFolder, start);
-   new Thread(resizer1).start();
+    for (int i = 0; i < cores; i++)
+    {
+        new Thread(resizer).start();
+    }
 
-   File[] files2 = new File[quarter];
-   System.arraycopy(files, quarter, files2, 0, files2.length);
-   ImageResizer resizer2 = new ImageResizer(files2, newWidth, dstFolder, start);
-   new Thread(resizer2).start();
 
-   File[] files3 = new File[quarter];
-   System.arraycopy(files, middle, files3, 0, files3.length);
-   ImageResizer resizer3 = new ImageResizer(files3, newWidth, dstFolder, start);
-   new Thread(resizer3).start();
 
-   File[] files4 = new File[quarter + files.length % 4];
-   System.arraycopy(files, third, files4, 0, files4.length);
-   ImageResizer resizer4 = new ImageResizer(files4, newWidth, dstFolder, start);
-   new Thread(resizer4).start();
+
+//    int quarter = (files.length - files.length % 4) / 4;
+//    int third = quarter * 3;
+//    int middle = quarter * 2;
+//
+//   File[] files1 = new File[quarter];
+//   System.arraycopy(files, 0, files1, 0, files1.length);
+//   ImageResizer resizer1 = new ImageResizer(files1, newWidth, dstFolder, start);
+//   new Thread(resizer1).start();
+//
+//   File[] files2 = new File[quarter];
+//   System.arraycopy(files, quarter, files2, 0, files2.length);
+//   ImageResizer resizer2 = new ImageResizer(files2, newWidth, dstFolder, start);
+//   new Thread(resizer2).start();
+//
+//   File[] files3 = new File[quarter];
+//   System.arraycopy(files, middle, files3, 0, files3.length);
+//   ImageResizer resizer3 = new ImageResizer(files3, newWidth, dstFolder, start);
+//   new Thread(resizer3).start();
+//
+//   File[] files4 = new File[quarter + files.length % 4];
+//   System.arraycopy(files, third, files4, 0, files4.length);
+//   ImageResizer resizer4 = new ImageResizer(files4, newWidth, dstFolder, start);
+//   new Thread(resizer4).start();
 
 
 
